a3238aa3003f7c4881cf01bb06d358f3b5b4346d
truetrue <dthnhung12k25@gmail.com>
2026-04-08 23:09:36 +0700

    08/04

---
+++ pages/HRM/employee_list.ts
@@ -0,0 +1,27 @@
+import {Page, Locator} from "@playwright/test";
+export class employee_list_Page{
+    readonly page : Page;
+    readonly btn_create: Locator;
+    readonly btn_import : Locator;
+    readonly btn_export : Locator;
+    readonly tab_working : Locator;
+    readonly tab_temporary_off_work : Locator;
+    readonly tab_quit: Locator;
+    readonly tab_all : Locator;
+
+    constructor (page : Page){
+        this.page = page;
+        this.btn_create = page.locator('button',{hasText:/Thêm mới/i});
+        this.btn_export = page.locator('button',{hasText:/Xuất excel/i});
+        this.btn_import = page.locator('button',{hasText:/Nhập excel/i});
+        this.tab_working = page.locator('div',{hasText:/Đang làm/i});
+        this.tab_temporary_off_work = page.locator('div',{hasText:/Tạm nghỉ/i});
+        this.tab_quit = page.locator('div',{hasText:/Nghỉ việc/i});
+        this.tab_all = page.locator('div',{hasText:/Tất cả/i});
+        }
+    async goto(){
+        await this.page.goto("/hrm/employee");
+    }
+}
+
+
---
+++ tests/QLCV/project-report/meeting-note.spec.ts
@@ -0,0 +1,8 @@
+import {test, expect} from "@playwright/test";
+
+import {meetingNote} from "../../../pages/Project_Manager/project-report/meetingNote_Page";
+
+test.describe("Create Meeting Note",()=>{
+    test.use({storageState: 'storageState.json'});
+    test()
+})
